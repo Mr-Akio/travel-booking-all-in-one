d8fee2de4220cad7c9c0a813690922d48f5077c5
Mr-Akio <139813694+Mr-Akio@users.noreply.github.com>
2026-05-14 20:29:52 +0700

    fix: use port 465 and SSL for SMTP to improve cloud compatibility

--- backend/travel_backend/settings.py
+++ backend/travel_backend/settings.py
@@ -164,8 +164,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'travelbooking0000@gmail.com'  
 EMAIL_HOST_PASSWORD = 'mkgs pukv joyg vugw'  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
